a7d18873f939bebf092a77527753ca80ef6b1944
Rathma <wrathofrathma@gmail.com>
2019-12-05 19:34:54 -0500

    o.o

--- motd.sh
+++ motd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export TERM=xterm-256color
 read one five fifteen rest < /proc/loadavg
-SERVICES=(nginx plexmediaserver sshd syncthing@rathma docker mariadb)
+SERVICES=(nginx plexmediaserver sshd syncthing@rathma docker mariadb vncserver@:1)
 clear
 chafa /opt/dotfiles/1n39ca69g8e21.png
 figlet -f slant "Welcome to ${HOSTNAME}" | lolcat -f
